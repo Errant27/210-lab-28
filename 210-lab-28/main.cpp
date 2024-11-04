8cf04243cfbabfbecf73afc7361f54bb98ca93a4
Errant27 <temporal.cerauno@gmail.com>
2024-11-03 21:22:19 -0800

    commit 6

--- 210-lab-28/main.cpp
+++ 210-lab-28/main.cpp
@@ -1,3 +1,5 @@
+// 210 | Lab 28 | Neil Orton
+// IDE Used: Xcode
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -15,16 +17,14 @@ void add_goat(list<Goat> &trip, string [], string []);
 void display_trip(list<Goat> trip);
 int main_menu();
 
-void merge_lists(list<Goat> &trip, string [], string []);
-void fill_list(list<Goat> &trip, string [], string []);
-void reverse_list(list<Goat> &trip);
-void clear_list(list<Goat> &trip);
-void sort_list(list<Goat> &trip);
-void replace_data(list<Goat> &trip);
-void get_unique(list<Goat> &trip);
-void find_Goat(list<Goat> trip);
-
-
+void merge_lists(list<Goat> &trip, string [], string []);    // merge list prorotype
+void fill_list(list<Goat> &trip, string [], string []);    // fill list prorotype
+void reverse_list(list<Goat> &trip);    // reverse list prorotype
+void clear_list(list<Goat> &trip);    // clear list prorotype
+void sort_list(list<Goat> &trip);    // sort list prorotype
+void replace_data(list<Goat> &trip);    // replace element prorotype
+void get_unique(list<Goat> &trip);    // get unique prorotype
+void find_Goat(list<Goat> trip);    // find element in list prorotype
 
 int main() {
     srand(time(0));
@@ -55,9 +55,9 @@ int main() {
         trip.push_back(tmp);
     }
     
-    // Goat Manager 3001 Engine
+    // Goat Manager 3002 Engine
     int sel = main_menu();
-    while (sel != CHOICES) {
+    while (sel != CHOICES) {    // while loop to run the main menu oprions
         switch (sel) {
             case 1:
                 cout << "Adding a goat.\n";
@@ -72,28 +72,28 @@ int main() {
                 display_trip(trip);
                 break;
             case 4:
-                merge_lists(trip, names, colors);
+                merge_lists(trip, names, colors);    // case 4: merge list is run
                 break;
             case 5:
-                fill_list(trip, names, colors);
+                fill_list(trip, names, colors);    // case 5: fill list is run
                 break;
             case 6:
-                reverse_list(trip);
+                reverse_list(trip);    // case 6: reverse list is run
                 break;
             case 7:
-                clear_list(trip);
+                clear_list(trip);    // case 7: clear list is run
                 break;
             case 8:
-                sort_list(trip);
+                sort_list(trip);    // case 8: sort list is run
                 break;
             case 9:
-                replace_data(trip);
+                replace_data(trip);    // case 9: replace element run
                 break;
             case 10:
-                get_unique(trip);
+                get_unique(trip);    // case 10: get unique list is run
                 break;
             case 11:
-                find_Goat(trip);
+                find_Goat(trip);    // case 11: find element in list list is run
                 break;
             default:
                 cout << "Invalid selection.\n";
@@ -106,7 +106,7 @@ int main() {
 }
 
 int main_menu() {
-    cout << "*** GOAT MANAGER 3001 ***\n";
+    cout << "*** GOAT MANAGER 3002 ***\n";
     cout << "[1] Add a goat\n";
     cout << "[2] Delete a goat\n";
     cout << "[3] Display goats\n";
@@ -174,7 +174,7 @@ int select_goat(list<Goat> trip) {
 void merge_lists(list<Goat> &trip, string nms[], string cls[]) {
     list<Goat> trip2;
     
-    for (int i = 0; i < trip.size(); i++) {
+    for (int i = 0; i < trip.size(); i++) {    // new list of goats made
       int age = rand() % MAX_AGE;
       string nm = nms[rand() % SZ_NAMES];
       string cl = cls[rand() % SZ_COLORS];
@@ -182,7 +182,7 @@ void merge_lists(list<Goat> &trip, string nms[], string cls[]) {
     trip2.push_back(tmp);
     }
     
-    cout << "List to merge\n";
+    cout << "List to merge\n";    // new list printed
     int i = 1;
     for (auto gt : trip2) {
         cout << "\t"
@@ -191,44 +191,44 @@ void merge_lists(list<Goat> &trip, string nms[], string cls[]) {
         << " (" << gt.get_age()
         << ", " << gt.get_color() << ")\n";
     }
-    list<Goat> mergedList(trip.size() + trip2.size());
+    list<Goat> mergedList(trip.size() + trip2.size());    // merged list size initialized
     
     cout << "Merging list...\n";
-    merge(trip.begin(), trip.end(), trip2.begin(), trip2.end(), mergedList.begin());
-    trip = mergedList;
+    merge(trip.begin(), trip.end(), trip2.begin(), trip2.end(), mergedList.begin());    // lists merged
+    trip = mergedList;    // Goat list assigned to the merged list
     cout << "Merging complete\n";
-    display_trip(trip);
+    display_trip(trip);    // newly merged list displayed
 }
 
 void fill_list(list<Goat> &trip, string nms[], string cls[]) {
     int age = rand() % MAX_AGE;
     string nm = nms[rand() % SZ_NAMES];
     string cl = cls[rand() % SZ_COLORS];
-    Goat tmp(nm, age, cl);
+    Goat tmp(nm, age, cl);    // temporary Goat object constructed
 
     cout << "Filling list with new Goat...\n";
-    fill(trip.begin(), trip.end(), tmp);
+    fill(trip.begin(), trip.end(), tmp);     // list filled with temporary Goat object
     cout << "Complete\n";
-    display_trip(trip);
+    display_trip(trip);    // filled list displayed
 }
 
 void reverse_list(list<Goat> &trip) {
     cout << "Reversing list...\n";
-    reverse(trip.begin(), trip.end());
+    reverse(trip.begin(), trip.end());    // list reversed via reverse algorithm function
     cout << "Complete\n";
-    display_trip(trip);
+    display_trip(trip);    // reversed list displayed
 }
 
 void clear_list(list<Goat> &trip) {
         cout << "Clearing...\n";
-        trip.clear();
+        trip.clear();    // list cleared via clear algorithm function
         cout << "List of Goats is now " << trip.size() << endl;
         cout << "----------------------------" << endl;
 }
 
 void sort_list(list<Goat> &trip) {
     cout << "Sorting...\n";
-    trip.sort();
+    trip.sort();    // list sorteded via sort algorithm function
     cout << "Complete\n";
     display_trip(trip);
 }
@@ -241,7 +241,7 @@ void replace_data(list<Goat> &trip) {
     string color1;
     string color2;
     
-    cout << "Enter Goat data you want to replace\n";
+    cout << "Enter Goat data you want to replace\n";    // user enters the name, age, and color of Goat they want to replace
     cout << "Name: ";
     cin >> name1;
     cout << "Age: ";
@@ -249,7 +249,7 @@ void replace_data(list<Goat> &trip) {
     cout << "Color: ";
     cin >> color1;
     
-    cout << "Enter Goat data you want to add\n";
+    cout << "Enter Goat data you want to add\n";    // user enters the name, age, and color of Goat they want to add
     cout << "Name: ";
     cin >> name2;
     cout << "Age: ";
@@ -257,17 +257,17 @@ void replace_data(list<Goat> &trip) {
     cout << "Color: ";
     cin >> color2;
     
-    Goat goat1(name1, age1, color1);
+    Goat goat1(name1, age1, color1);    // Goat objects constructed with inputs as arguments
     Goat gaot2(name2, age2, color2);
-    replace(trip.begin(), trip.end(), goat1, gaot2);
+    replace(trip.begin(), trip.end(), goat1, gaot2);    // replace function replaces the respective Goat in the list
     display_trip(trip);
 }
 
 void get_unique(list<Goat> &trip) {
     sort_list(trip);
     cout << "Removing duplicate values...\n";
-        auto newEnd = unique(trip.begin(), trip.end());
-        trip.erase(newEnd, trip.end());
+        auto newEnd = unique(trip.begin(), trip.end());    // unique checks duplicates
+        trip.erase(newEnd, trip.end());    // erase removes duplicates
     cout << "Complete\n";
         display_trip(trip);
 }
@@ -277,19 +277,19 @@ void find_Goat(list<Goat> trip) {
     string name;
     string color;
 
-    cout << "Name to search: ";
+    cout << "Name to search: ";    // user enters the name, age, and color of Goat they want to search
     cin >> name;
     cout << "Age to search: ";
     cin >> age;
     cout << "Color to search: ";
     cin >> color;
-    Goat tmp(name, age, color);
+    Goat tmp(name, age, color);    // Goat objects constructed with inputs as arguments
 
-    auto it = find(trip.begin(), trip.end(), tmp);
+    auto it = find(trip.begin(), trip.end(), tmp);    // iterator moves to tmp Goat object
     if (it != trip.end()) {
-        cout << "Found the Goat: " << it->get_name() << " (" << it->get_age() << ", " << it->get_color() << ")"<< endl;
+        cout << "Found the Goat: " << it->get_name() << " (" << it->get_age() << ", " << it->get_color() << ")"<< endl;    // Goat object outputted if found
     } else {
-        cout << "Goat not found\n";
+        cout << "Goat not found\n";    // messae if Goat object is not found
     }
     cout << "----------------------------" << endl;
 }
